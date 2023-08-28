e10c5b3a7805900f9452f03e2f1ab46c8c605b5c
Rafał Klocek <rafal.klocek.praca@gmail.com>
2023-08-28 20:47:20 +0200

    added exchange function

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 const footerYear = document.querySelector(".footer__year");
 const coinsList = document.querySelector(".main__coins");
+const coinsExchange = document.querySelector(".main__input");
+const selectSecond = document.querySelector("#selectSecond");
+const selectFirst = document.querySelector("#selectFirst");
+const select = document.querySelectorAll(".main__select");
+const exchangeValue = document.querySelector(".main__exchange-right-value");
+const exchangeValueIcon = document.querySelector(".main__value-pic");
 const coinColorOne = "rgb(53, 33, 184)";
 const coinColorTwo = "rgb(37, 23, 131)";
 const API = {
@@ -16,10 +22,10 @@ const request = fetch("https://api.coinranking.com/v2/coins", API)
 	.then((result) => {
 		coinsData.push(result.data.coins);
 		setCoins();
+		setEchangeOptions();
 	});
 
 const setCoins = () => {
-	console.log(coinsData[0][0]);
 	for (let i = 0; i < 10; i++) {
 		const number = i % 2;
 		const coinItem = document.createElement("li");
@@ -47,9 +53,65 @@ const setCoins = () => {
 	}
 };
 
+const setEchangeOptions = () => {
+	for (let i = 0; i < 10; i++) {
+		const selectOption = document.createElement("option");
+		const optionText = document.createElement("p");
+		selectOption.setAttribute("value", i);
+		optionText.setAttribute("class", "main__option-text");
+		optionText.textContent = coinsData[0][i].symbol;
+		selectOption.append(optionText);
+		selectSecond.append(selectOption);
+	}
+
+	for (let i = 0; i < 10; i++) {
+		const selectOption = document.createElement("option");
+		const optionText = document.createElement("p");
+		selectOption.setAttribute("value", i);
+		optionText.setAttribute("class", "main__option-text");
+		optionText.textContent = coinsData[0][i].symbol;
+		selectOption.append(optionText);
+		selectFirst.append(selectOption);
+	}
+
+	select.forEach((item) => {
+		item.addEventListener("click", setExchange);
+	});
+};
+
+const setExchange = () => {
+	if (coinsExchange.value.length > 7) {
+        coinsExchange.value = coinsExchange.value.slice(0,7); 
+    }
+
+	if (coinsExchange.value !== "") {
+		if (selectFirst.value === "EURO") {
+			exchangeValue.textContent = `${parseFloat(
+				coinsExchange.value / coinsData[0][selectSecond.value].price
+			).toFixed(9)}`;
+		} else if (selectFirst.value !== "EURO") {
+			exchangeValue.textContent = `${parseFloat(
+				(coinsExchange.value * coinsData[0][selectFirst.value].price) /
+					coinsData[0][selectSecond.value].price
+			).toFixed(9)}`;
+		}
+	}
+
+	exchangeValueIcon.setAttribute(
+		"src",
+		coinsData[0][selectSecond.value].iconUrl
+	);
+	exchangeValueIcon.setAttribute(
+		"alt",
+		coinsData[0][selectSecond.value].symbol
+	);
+};
+
 const setFooterYear = () => {
 	const date = new Date();
 	footerYear.textContent = date.getFullYear();
 };
 
 setFooterYear();
+
+coinsExchange.addEventListener("keyup", setExchange);
