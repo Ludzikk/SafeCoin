2d4da4698483a09733c6ca1eab72b458779d327b
Rafał Klocek <rafal.klocek.praca@gmail.com>
2023-08-30 15:50:12 +0200

    added rwd

---
+++ src/js/mobilenav.js
@@ -0,0 +1,19 @@
+const hamburgerBtn = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".header__nav");
+const mobileLink = document.querySelectorAll(".header__link");
+
+const addListeners = () => {
+	hamburgerBtn.addEventListener("click", toggleMobileNav);
+
+	mobileLink.forEach((link) => {
+		link.addEventListener("click", toggleMobileNav);
+	});
+};
+
+const toggleMobileNav = () => {
+	mobileNav.classList.toggle("hidden");
+	hamburgerBtn.classList.toggle("is-active");
+	document.body.classList.toggle("hidden-body");
+};
+
+addListeners();
