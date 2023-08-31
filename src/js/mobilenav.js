8e3e5c261316c2a52c6ab7309e7f692e90167fd6
Rafał Klocek <rafal.klocek.praca@gmail.com>
2023-08-31 16:00:11 +0200

    changed colors, and added animations

--- src/js/mobilenav.js
+++ src/js/mobilenav.js
@@ -1,6 +1,7 @@
 const hamburgerBtn = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".header__nav");
 const mobileLink = document.querySelectorAll(".header__link");
+const arrow = document.querySelector(".fa-arrow-right-arrow-left");
 
 const addListeners = () => {
 	hamburgerBtn.addEventListener("click", toggleMobileNav);
@@ -11,9 +12,24 @@ const addListeners = () => {
 };
 
 const toggleMobileNav = () => {
-	mobileNav.classList.toggle("hidden");
-	hamburgerBtn.classList.toggle("is-active");
-	document.body.classList.toggle("body-hidden");
+	if (!mobileNav.classList.contains("hidden")) {
+		mobileNav.classList.add("hide-nav");
+		arrow.classList.toggle("hide");
+		hamburgerBtn.classList.toggle("is-active");
+		document.body.classList.toggle("body-hidden");
+		setTimeout(() => {
+			mobileNav.classList.remove("hide-nav");
+			mobileNav.classList.remove("show-nav");
+			mobileNav.classList.toggle("hidden");
+		}, 500);
+	} else {
+		hamburgerBtn.classList.toggle("is-active");
+		arrow.classList.toggle("hide");
+		mobileNav.classList.toggle("hidden");
+		mobileNav.classList.add("show-nav");
+		document.body.classList.toggle("body-hidden");
+		mobileNav.classList.remove("hide-nav");
+	}
 };
 
 addListeners();
